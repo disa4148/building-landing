2294d56c478f97ccbd366e6f63ec6476f33797cd
disa4148 <denis.nikolaenko.2004@mail.ru>
2025-07-14 01:53:33 +0700

    feat: features-section

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,6 +2,7 @@ import PromoBanner from '@/src/widgets/PromoBanner/PromoBanner';
 import About from '@/src/widgets/About/About';
 import WorksCarousel from '@/src/widgets/WorksCarousel/WorksCarousel';
 import WorkProcess from '@/src/widgets/WorksProcess/WorksProcess';
+import Features from '@/src/widgets/Features/Features';
 
 const Home: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const Home: React.FC = () => {
       <About />
       <WorksCarousel />
       <WorkProcess />
+      <Features />
     </>
   );
 };
---
+++ src/widgets/Features/Features.tsx
@@ -0,0 +1,40 @@
+import Title from '@/src/shared/ui/Title/Title';
+
+import css from './Features.module.scss';
+
+const features = [
+  {
+    title: 'Качество',
+    text: 'Используем проверенные материалы и строго соблюдаем технологию',
+  },
+  {
+    title: 'Особый подход',
+    text: 'Создаем уникальные текстуры под любой стиль',
+  },
+  {
+    title: 'Надежность',
+    text: 'Гарантируем прочность и долговечность покрытий',
+  },
+  {
+    title: 'Современные решения',
+    text: 'Реализуем любые дизайнерские идеи',
+  },
+];
+
+const Features = (): JSX.Element => (
+  <section className={css.section}>
+    <div className={css.container}>
+      <Title color="secondary">Наши преимущества</Title>
+      <div className={css.grid}>
+        {features.map((f, i) => (
+          <div className={css.card} key={i}>
+            <div className={css.title}>{f.title}</div>
+            <div className={css.text}>{f.text}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+);
+
+export default Features;
